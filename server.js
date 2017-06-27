1b967d6bd2f7b585eda11105e9536e9032065a73
Jonathan Lowell <elderlowell@gmail.com>
2017-06-27 17:59:47 -0600

    Completed mini Siri project.

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+
+var app = express();
+
+app.listen(8887, function() {
+  console.log('Listening on port 8887.');
+});
+
+
+var messages = ["Hello.", "I don't understand what you want me to do.", "I can tell you how to do that...next week.", "Figure it out yourself."];
+
+var getRandomMessage = function() {
+  return messages[Math.floor(Math.random() * messages.length)];
+};
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage()
+  }));
+});
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
